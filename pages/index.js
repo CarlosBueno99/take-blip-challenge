5feae7c768b7ee5f5dfb06a1b7be4ad29733366b
CarlosBueno99 <carlosbueno.contato@gmail.com>
2021-08-05 23:50:51 -0300

    inicio do negocio do next, depois vou fazer o endpoint da API que vou colocar na vercel

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <div>Então quer dizer que vc veio aqui curiar o que tinha na página index né? Certo, acontece, seja bem vindo. Aqui não tem nada demais</div>
+}
+
+export default Home
