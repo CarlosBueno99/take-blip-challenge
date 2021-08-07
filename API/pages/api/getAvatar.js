135e58fc0dffb56f2ea7ab48ef612672d507bfee
CarlosBueno99 <carlosbueno.contato@gmail.com>
2021-08-06 21:12:47 -0300

    added the getAvatar endpoint

---
+++ API/pages/api/getAvatar.js
@@ -0,0 +1,15 @@
+async function getAvatar(request,response){
+
+    const idCard = 4;
+
+    const githubRepoResponse = await fetch("https://take-blip-challenge.vercel.app/api/blipRepo");
+    const gitData = await githubRepoResponse.json();
+
+    console.log("there you go boy");
+
+    response.json(
+        gitData[idCard].avatar
+    )
+}
+
+export default getAvatar
